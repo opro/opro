91d070cead364a3a8e81f5455da56d83459d98a4
schneems <richard.schneeman@gmail.com>
2012-04-09 13:30:05 -0500

    helper methods for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -24,7 +24,75 @@ Capybara.default_selector = :css
 
 
 # Run any available migration
+ActiveRecord::Migration.verbose = false
+ActiveRecord::Base.logger = Logger.new(nil)
+
+
 ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__)
+
+class ActiveSupport::TestCase
+  self.use_transactional_fixtures = true
+  self.use_instantiated_fixtures  = false
+end
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+
+
+include Devise::TestHelpers
+
+# gives us the login_as(@user) method when request object is not present
+include Warden::Test::Helpers
+Warden.test_mode!
+
+def rand_name
+  'foo' + Time.now.to_f.to_s
+end
+
+
+def create_user(options = {})
+  User.create(:email => rand_name + '@bar.com', :password => 'password', :password_confirm => 'password')
+end
+
+def create_client_app(options= {})
+  user = options[:user] || create_user
+  name = options[:name] || rand_name
+  Oauth::ClientApplication.create_with_user_and_name(user, name)
+end
+
+def user_with_client_app
+  user = create_user
+  create_client_app(:user => user)
+  user
+end
+
+def create_auth_grant_for_user(user, app = nil)
+  app ||= create_client_app
+  Oauth::AccessGrant.create(:user => user, :application => app)
+end
+
+# Will run the given code as the user passed in
+def as_user(user=nil, &block)
+  current_user = user || create_user
+  if self.respond_to? :request
+    sign_in(current_user)
+  else
+    login_as(current_user, :scope => :user)
+  end
+  block.call if block.present?
+  return self
+end
+
+
+def as_visitor(user=nil, &block)
+  current_user = user || create_user
+  if self.respond_to? :request
+    sign_out(current_user)
+  else
+    logout(:user)
+  end
+  block.call if block.present?
+  return self
+end
+
