762b5a0cc164c92655f9231686a069ba94707757
schneems <richard.schneeman@gmail.com>
2012-04-12 11:30:15 -0500

    prevent duplicate client applications from being created

--- app/controllers/oauth/client_application_controller.rb
+++ app/controllers/oauth/client_application_controller.rb
@@ -6,7 +6,13 @@ class Oauth::ClientApplicationController < ApplicationController
   end
 
   def create
-    @client_app = Oauth::ClientApplication.create_with_user_and_name(current_user, params[:oauth_client_application][:name])
+    @client_app = Oauth::ClientApplication.find_by_user_id_and_name(current_user.id, params[:oauth_client_application][:name])
+    @client_app ||= Oauth::ClientApplication.create_with_user_and_name(current_user, params[:oauth_client_application][:name])
+    if @client_app.save
+      # do nothing
+    else
+      render :new
+    end
   end
 
   def index
