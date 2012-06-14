cbfb615aa7b71af0d9f12e2e99ed51649c608996
schneems <richard.schneeman@gmail.com>
2012-06-14 11:47:18 -0700

    fix markdown/oauth.md.erb and add tests

--- test/integration/docs_controller_test.rb
+++ test/integration/docs_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class DocsControllerTest < ActiveSupport::IntegrationCase
-  test 'renders' do
+  test 'renders index' do
     visit oauth_docs_path
     assert_equal '/oauth_docs', current_path
+  end
+
+  test 'renders show' do
+    [:curl, :oauth, :quick_start].each do |doc|
+      doc_path = oauth_doc_path(:id => doc)
+      visit doc_path
+      assert_equal doc_path, current_path
+    end
   end
 end
