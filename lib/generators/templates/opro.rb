a2faa1104996b26e51668d884e5625ede1e3b61e
schneems <richard.schneeman@gmail.com>
2012-06-18 03:09:53 -0500

    update template

--- lib/generators/templates/opro.rb
+++ lib/generators/templates/opro.rb
@@ -3,14 +3,15 @@ Opro.setup do |config|
   config.auth_strategy = :devise
 
   ## Add or remove application permissions
-  # Read permission is turned on by default (any request with [GET])
-  # Write permission is requestable by default (any request other than [GET])
-  # Custom permissions can be configured by adding them to the request_permissions Array and configuring require_oauth_permissions in the controller
+  # Read permission (any request with [GET]) is turned on by default
+  # Write permission (any request other than [GET]) is requestable by default
+  # Custom permissions can be configured by adding them to `config.request_permissions`
+  # You can then require that permission on individual actions by calling
+  # `require_oauth_permissions` in the controller
   config.request_permissions = [:write]
 
   ## Refresh Token config
   # uncomment `config.require_refresh_within` to require refresh tokens
   # this will expire tokens within the given time duration
-  # tokens can be refreshed using the refresh toke
   # config.require_refresh_within = 1.month
 end
