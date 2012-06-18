0e8c60941ee28d8fff8724ede8a179053b48ecc1
schneems <richard.schneeman@gmail.com>
2012-06-18 03:09:40 -0500

    remove un-used file

--- app/controllers/opro_application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-
-  # Any code that would/should go into ApplicationController is
-  # now in lib/opro/conrollers/application_controller_helper.rb
-  # it is loaded into ApplicationController in lib/opro/engine.rb
-  # thanks for visiting, come back soon
-
-end
