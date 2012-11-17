42bf8261f958b231b0556de72047b12fe806e6df
schneems <richard.schneeman@gmail.com>
2012-11-17 16:20:16 -0500

    refactor tests controller

--- app/controllers/opro/oauth/tests_controller.rb
+++ app/controllers/opro/oauth/tests_controller.rb
@@ -7,15 +7,10 @@ class Opro::Oauth::TestsController < OproController
   end
 
   def show
-    result = if valid_oauth?
-      {:status => 200, :message => 'OAuth worked!', :params => params, :user_id => oauth_user.id }
-    else
-      {:status => :unauthorized, :message => "OAuth did not work :(  #{generate_oauth_error_message!}", :params => params}
-    end
-
+    result = oauth_result(params)
     respond_to do |format|
       format.html do
-        render :text => result.to_json,   :status => result[:status], :layout => true
+        render :text => result.to_json, :status => result[:status], :layout => true
       end
       format.json do
         render :json => result, :status => result[:status]
@@ -24,15 +19,10 @@ class Opro::Oauth::TestsController < OproController
   end
 
   def create
-    result = if valid_oauth?
-      {:status => 200, :message => 'OAuth worked!', :params => params, :user_id => oauth_user.id }
-    else
-      {:status => :unauthorized, :message => "OAuth did not work :(  #{generate_oauth_error_message!}", :params => params}
-    end
-
+    result = oauth_result(params)
     respond_to do |format|
       format.html do
-        render :text => result.to_json,   :status => result[:status], :layout => true
+        render :text => result.to_json, :status => result[:status], :layout => true
       end
       format.json do
         render :json => result, :status => result[:status]
@@ -54,6 +44,16 @@ class Opro::Oauth::TestsController < OproController
       format.json do
         render :json => result, :status => result[:status]
       end
+    end
+  end
+
+  private
+
+  def oauth_result(options)
+    if valid_oauth?
+      {:status => 200, :message => 'OAuth worked!', :params => options, :user_id => oauth_user.id }
+    else
+      {:status => :unauthorized, :message => "OAuth did not work :(  #{generate_oauth_error_message!}", :params => params}
     end
   end
 end
