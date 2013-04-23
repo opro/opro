d85c93839ab954c809d42227784fbb9f952856a3
chatgris <jboyer@af83.com>
2013-04-23 15:45:47 +0900

    Remove duplication in client_app_controller
    Signed-off-by: chatgris <jboyer@af83.com>

--- app/controllers/opro/oauth/client_app_controller.rb
+++ app/controllers/opro/oauth/client_app_controller.rb
@@ -1,5 +1,6 @@
 class Opro::Oauth::ClientAppController < OproController
   before_filter :opro_authenticate_user!
+  before_filter :client_app, only: [:show, :edit, :update, :create]
 
   def new
     @client_app = Opro::Oauth::ClientApp.new
@@ -11,15 +12,12 @@ class Opro::Oauth::ClientAppController < OproController
   end
 
   def show
-    @client_app = Opro::Oauth::ClientApp.where(id: params[:id], user_id: current_user.id).first
   end
 
   def edit
-    @client_app = Opro::Oauth::ClientApp.where(id: params[:id], user_id: current_user.id).first
   end
 
   def update
-    @client_app = Opro::Oauth::ClientApp.where(id: params[:id], user_id: current_user.id).first
     @client_app.name = params[:opro_oauth_client_app][:name]
     if @client_app.save
       redirect_to oauth_client_app_path(@client_app)
@@ -28,9 +26,7 @@ class Opro::Oauth::ClientAppController < OproController
     end
   end
 
-
   def create
-    @client_app = Opro::Oauth::ClientApp.find_by_user_id_and_name(current_user.id, params[:opro_oauth_client_app][:name])
     @client_app ||= Opro::Oauth::ClientApp.create_with_user_and_name(current_user, params[:opro_oauth_client_app][:name])
     if @client_app.save
       redirect_to oauth_client_app_path(@client_app)
@@ -39,4 +35,7 @@ class Opro::Oauth::ClientAppController < OproController
     end
   end
 
+  def client_app
+    @client_app ||= Opro::Oauth::ClientApp.where(id: params[:id], user_id: current_user.id).first
+  end
 end
