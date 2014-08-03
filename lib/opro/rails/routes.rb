80e035fb399b32d8760b6d08263d91064d8406b2
reignmaker <reignmaker@xakep.ru>
2014-08-03 14:52:34 +0400

    additional controller options

--- lib/opro/rails/routes.rb
+++ lib/opro/rails/routes.rb
@@ -7,9 +7,11 @@ module ActionDispatch::Routing
       controllers = options[:controllers] || {}
 
       oauth_new_controller = controllers[:oauth_new] || 'opro/oauth/auth'
+      oauth_create_controller = controllers[:oauth_create] || 'opro/oauth/auth'
+      oauth_token_controller = controllers[:oauth_token] || 'opro/oauth/token'
       get  'oauth/new'          => "#{oauth_new_controller}#new",  :as => 'oauth_new'
-      post 'oauth/authorize'    => 'opro/oauth/auth#create',       :as => 'oauth_authorize'
-      post 'oauth/token'        => 'opro/oauth/token#create',      :as => 'oauth_token'
+      post 'oauth/authorize'    => "#{oauth_create_controller}#create",       :as => 'oauth_authorize'
+      post 'oauth/token'        => "#{oauth_token_controller}#create",      :as => 'oauth_token'
 
       unless skip_routes.include?(:client_apps)
         oauth_client_apps = controllers[:oauth_client_apps] ||'opro/oauth/client_app'
